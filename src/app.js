69d557041fbcdaa2bd7cd391138053d1b7566fcc
Dardev <sbadal745@gmail.com>
2025-07-16 07:14:01 +0530

    util code(apiError and apiResponse)

--- src/app.js
+++ src/app.js
@@ -1,5 +1,20 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+app.use(cors({
+  origin: process.env.CORS_ORIGIN, //defining origin from app is allowed to access
+  credentials:true
+}))
+
+app.use(express.json({limit:"16kb"}))  //for taking data from json
+
+app.use(express.urlencoded({extended:true, limit: "16kb"})) //for taking data from url
+
+app.use(express.static("public"))   //for storing file and folder in public folder
+
+app.use(cookieParser()) //for accesing cookie and set cookie
 
 export { app }
